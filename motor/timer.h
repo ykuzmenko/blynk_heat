56c414286b810b21a5ad3c4ac2dbaa5de5334bf6
Yaroslav Kuzmenko <yaroslav.kuzmenko@gmail.com>
2018-02-21 18:12:43 +0200

    Add weekday check

--- motor/timer.h
+++ motor/timer.h
@@ -4,10 +4,13 @@
 WidgetRTC rtc;
 
 class MyTimer {
-		int startHour;
-		int startMinute;
-		int stopHour;
-		int stopMinute;
+    int startHour;
+    int startMinute;
+    int stopHour;
+    int stopMinute;
+    byte weekdays = 0x00;
+
+    // weekday(); // Day of the week (1-7), Sunday is day 1
 
     public:
         MyTimer() {
@@ -15,38 +18,39 @@ class MyTimer {
         }
 
         void init_from_timeinput(TimeInputParam t) {
-        	if (t.hasStartTime()) {
-        		startHour = t.getStartHour();
-        		startMinute = t.getStartMinute();
-  				}
+          if (t.hasStartTime()) {
+            startHour = t.getStartHour();
+            startMinute = t.getStartMinute();
+          }
+
+          if (t.hasStopTime()) {
+            stopHour = t.getStopHour();
+            stopMinute = t.getStopMinute();
+          }
+
+          for (int day = 1; day < 8; day++) {
+            if (t.isWeekdaySelected(day)) {
+              bitSet(weekdays, day);
+            }
+          }
         }
 
-        void setStartTime(int startHour, int startMinute) {
-        	startHour = startHour;
-        	startMinute = startMinute;
-        }
+        // void setStartTime(int startHour, int startMinute) {
+        //   startHour = startHour;
+        //   startMinute = startMinute;
+        // }
         
-        void setStopTime(int stopHour, int stopMinute) {
-					stopHour = stopHour;
-					stopMinute = stopMinute;
-        }
-
-        int getStartTimeHour(void) {
-        	return(stopHour);
-        }
-
-        int getStartTimeMinute(void) {
-        	return(stopHour);
-        }
+        // void setStopTime(int stopHour, int stopMinute) {
+        //   stopHour = stopHour;
+        //   stopMinute = stopMinute;
+        // }
 
         bool checkStartTime() {
-        	weekday();
-        	return hour() == startHour && minute() == startMinute;
+          return hour() == startHour && minute() == startMinute && bitRead(weekdays, (weekday() - 1) % 7);
         }
 
         bool checkStopTime() {
-        	weekday();
-        	return hour() == stopHour && minute() == stopMinute;
+          return hour() == stopHour && minute() == stopMinute && bitRead(weekdays, (weekday() - 1) % 7);
         }
 };
 
