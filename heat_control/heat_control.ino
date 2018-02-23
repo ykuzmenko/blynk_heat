bed7c1de748b63bc74d189809259a038ab2fa105
Yaroslav Kuzmenko <yaroslav.kuzmenko@gmail.com>
2018-02-23 16:03:19 +0200

    Add log to terminal more informative

--- heat_control/heat_control.ino
+++ heat_control/heat_control.ino
@@ -24,6 +24,13 @@ WidgetTerminal terminal(V10);
 
 MyMotor my_motor = MyMotor(7, 6, 5, 9, 8);
 
+byte arduino_mac[] = { 0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
+IPAddress arduino_ip ( 192,   168,   2,  2);
+IPAddress dns_ip     (  8,   8,   8,   8);
+IPAddress gateway_ip ( 192,   168,   2,   1);
+IPAddress subnet_mask(255, 255, 255,   0);
+
+
 void setup () {
   pinMode(SDCARD_CS, OUTPUT);
   digitalWrite(SDCARD_CS, HIGH); // Deselect the SD card
@@ -32,7 +39,8 @@ void setup () {
 
   timer.setInterval(300L, motor_process);
 
-  Blynk.begin(auth, blynk_server, 8442);
+  // Blynk.begin(auth, blynk_server, 8442);
+  Blynk.begin(auth, blynk_server, 8442, arduino_ip, dns_ip, gateway_ip, subnet_mask, arduino_mac);
 
   setSyncInterval(10 * 60);
   timer.setInterval(60000L, check_my_timers);
@@ -43,10 +51,12 @@ BLYNK_WRITE(V1) {
   int buttonState = param.asInt();
 
   if (buttonState == 0) {
+    log_to_terminal("manually close");
     my_motor.close();
   }
 
   if (buttonState == 1) {
+    log_to_terminal("manually open");
     my_motor.open();
   }  
 }
@@ -54,8 +64,13 @@ BLYNK_WRITE(V1) {
 BLYNK_WRITE(V2) {
   TimeInputParam t(param);
 
+  BLYNK_LOG("Timer1 set");
+
   MyTimer mt = MyTimer();
   mt.init_from_timeinput(t);
+
+  log_to_terminal("Timer1 set");
+  terminal.flush();
 
   my_timers[0] = mt;
 }
@@ -67,13 +82,17 @@ BLYNK_WRITE(V3) {
   MyTimer mt = MyTimer();
   mt.init_from_timeinput(t);
 
+  log_to_terminal("Timer2 set");
+
   my_timers[1] = mt;
 
 }
 
 BLYNK_CONNECTED() {
-  Blynk.syncVirtual(V1, V2, V3, V10);
+  Blynk.syncVirtual(V1, V2, V3);
   rtc.begin();
+  log_to_terminal("Connected");
+  terminal.flush();
 }
 
 void motor_process() {
@@ -81,25 +100,33 @@ void motor_process() {
 }
 
 void check_my_timers() {
- for (int i = 0; i < 2; i++) {
-   if (my_timers[i].checkStartTime()) {
-      terminal.print("Opened at: ");
-      terminal.print(hour());
-      terminal.print(":");
-      terminal.print(minute());
+  log_to_terminal("check timers");
 
-      my_motor.open();
-   }
-   if (my_timers[i].checkStopTime()) {
-      terminal.print("Closed at: ");
-      terminal.print(hour());
-      terminal.print(":");
-      terminal.print(minute());
+   for (int i = 0; i < 2; i++) {
+     if (my_timers[i].checkStartTime()) {
+        log_to_terminal("open by Timer");
 
-      my_motor.close();
+        Blynk.virtualWrite(1, 1);
+        my_motor.open();
+
+     }
+     if (my_timers[i].checkStopTime()) {
+        log_to_terminal("close by Timer");
+
+        Blynk.virtualWrite(1, 0);
+        my_motor.close();
+     }
    }
- }
  terminal.flush();
+}
+
+void log_to_terminal(String line) {
+  terminal.print(hour());
+  terminal.print(":");
+  terminal.print(minute());
+  terminal.print(" ");
+  terminal.println(line);
+  terminal.flush();
 }
 
 void loop(){
